40fdb1254f4f6c00a25f7b8b62f4bc3739942278
MU94W <nanqiao15@126.com>
2017-04-16 02:11:01 +0800

    ready to finish AttentionDecoderContainer

--- Decoder.py
+++ Decoder.py
@@ -85,7 +85,7 @@ class SimpleRNNCell(object):
                                                        initializer=self.peak_initializer,
                                                        regularizer=None,
                                                        constraint=None)
-    
+
     def step(self, x, peak, state):
         last_output = state
         output = K.dot(peak, self.peak_kernel) + K.dot(last_output, self.recurrent_kernel)
@@ -96,7 +96,7 @@ class SimpleRNNCell(object):
         if self.activation is not None:
             output = self.activation(output)
         return output, [output]
-    
+
     def getInitialState(self, peak):
         state = K.dot(peak, self.peak_initial)
         return [state]
@@ -236,7 +236,6 @@ class GRUCell(object):
             self.bias_r = None
             self.bias_h = None
 
-    #def step(self, inputs, states):
     def step(self, x, peak, state):
         h_tm1 = state # previous memory
 
@@ -250,7 +249,6 @@ class GRUCell(object):
         z = self.recurrent_activation(x_z + K.dot(h_tm1, self.recurrent_kernel_z) + K.dot(peak, self.peak_kernel_z))
         r = self.recurrent_activation(x_r + K.dot(h_tm1, self.recurrent_kernel_r) + K.dot(peak, self.peak_kernel_r))
 
-        #hh = self.activation(x_h + K.dot(r * h_tm1, self.recurrent_kernel_h))
         hh = self.activation(x_h + r * (K.dot(h_tm1, self.recurrent_kernel_h) + K.dot(peak, self.peak_kernel_h)))
         h = z * h_tm1 + (1 - z) * hh
         return h, [h]
@@ -262,6 +260,7 @@ class GRUCell(object):
 class DecoderContainer(Layer):
     """Decoder Container class
     # Properties
+        max_time_steps: Integer, which indicates the max length to decode.
         decoder_config: A list, which contains the configs of each recurrent layer stacked by the order in list.
                         Each config is a dictionary.
     """
@@ -372,17 +371,16 @@ class DecoderContainer(Layer):
         if isinstance(outputs, list):
             outputs = outputs[0]
         outputs = T.squeeze(outputs)
-        axes = [1,0] + list(range(2, outputs.ndim))
+        axes = [1, 0] + list(range(2, outputs.ndim))
         outputs = outputs.dimshuffle(axes)
         return outputs
 
     def build(self, input_shapes):
         """
         Assumption
-            
         """
         self.__addCell(input_shapes)
-    
+
     @classmethod
     def stack(cls, container, cell_obj):
         """
@@ -391,12 +389,196 @@ class DecoderContainer(Layer):
         raise NotImplementedError
 
     def compute_output_shape(self, input_shapes):
+        """
+        wait to comp
+        """
         if isinstance(input_shapes, list):
             return (input_shapes[0][0], self.max_time_steps, self.output_dim)
         else:
             return (input_shapes[0], self.max_time_steps, self.output_dim)
-   
+
     def call(self, inputs):
+        """
+        wait to comp
+        """
+        if self.cell_num == len(inputs):
+            _, peaks = self.__preprocessInputs(inputs)
+            initial_states = self.__getInitialStates(peaks)
+            outputs = self.__rnn(peaks, initial_states, None)
+        else:
+            _, peaks, indication = self.__preprocessInputs(inputs)
+            initial_states = self.__getInitialStates(peaks)
+            outputs = self.__rnn(peaks, initial_states, indication)
+        return outputs
+
+    def get_config(self):
+        """
+        wait to comp
+        """
+        config = {'max_time_steps': self.max_time_steps,
+                  'decoder_config': self.decoder_config}
+        base_config = super(DecoderContainer, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+class AttentionDecoderContainer(Layer):
+    """Attention Decoder Container class
+    # Properties
+        max_time_steps: Integer, which indicates the max length to decode.
+        decoder_config: A list, which contains the configs of each recurrent layer stacked by the order in list.
+                        Each config is a dictionary.
+    """
+    def __init__(self, max_time_steps, decoder_config, **kwargs):
+        self.max_time_steps = max_time_steps
+        self.decoder_config = decoder_config
+        self.output_dim = decoder_config[-1].get('units')
+        self.cell_num = len(decoder_config)
+        super(AttentionDecoderContainer, self).__init__(**kwargs)
+
+    def __addCell(self, input_shapes):
+        """
+        wait to comp
+        """
+        self.cells = []
+        if isinstance(input_shapes, list):
+            for index, item in enumerate(self.decoder_config):
+                cell_type = item.get('type')
+                peak_dim = input_shapes[index][-1]
+                if index is not 0:
+                    input_dim = self.decoder_config[index-1].get('units')
+                else:
+                    input_dim = self.decoder_config[-1].get('units')
+                name = self.name + '_inner_cell_%d_' % index + cell_type
+                self.cells.append(addCell(self, cell_type, peak_dim, input_dim, name, item))
+        else:
+            config = self.decoder_config[0]
+            cell_type = config.get('type')
+            peak_dim = input_shapes[-1]
+            input_dim = config.get('units')
+            name = '0_' + cell_type
+            self.cells.append(addCell(self, cell_type, peak_dim, input_dim, name, config))
+
+    def __preprocessInputs(self, inputs):
+        """
+        wait to comp
+        """
+        has_indi = False
+        if self.cell_num == len(inputs) - 1:
+            indication = inputs[-1]
+            ndim = indication.ndim
+            assert ndim >= 3, 'Indication should be at least 3D.'
+            axes = [1, 0] + list(range(2, ndim))
+            indication = indication.dimshuffle(axes)
+            inputs = inputs[:-1]
+            has_indi = True
+        top_encoder_out = inputs[-1]
+        ndim = top_encoder_out.ndim
+        assert ndim >= 3, 'top encoder output should be at least 3D.'
+        axes = [1, 0] + list(range(2, ndim))
+        top_encoder_out = top_encoder_out.dimshuffle(axes)
+        peaks = []
+        for item in inputs:
+            ndim = item.ndim
+            assert ndim >= 3, 'Input should be at least 3D.'
+            axes = [1, 0] + list(range(2, ndim))
+            item = item.dimshuffle(axes)
+            peaks.append(item[-1])
+        if has_indi:
+            return top_encoder_out, peaks, indication
+        else:
+            return top_encoder_out, peaks
+    
+    def __getInitialStates(self, peaks):
+        """
+        wait to comp
+        """
+        initial_states = []
+        for index in range(self.cell_num):
+            cell = self.cells[index]
+            peak = peaks[index]
+            state = cell.getInitialState(peak)
+            initial_states.extend(state)
+        return initial_states
+
+    def __step(self, time, output_tm1, *states_tm1_and_peaks):
+        """
+        wait to comp
+        """
+        states_tm1 = states_tm1_and_peaks[:self.cell_num]
+        peaks = states_tm1_and_peaks[self.cell_num:]
+        states = []
+        bottom_cell = self.cells[0]
+        peak = peaks[0]
+        state_tm1 = states_tm1[0]
+        output, state = bottom_cell.step(output_tm1, peak, state_tm1)
+        output_hm1 = output
+        states.extend(state)
+        for index in range(1, self.cell_num):
+            cell = self.cells[index]
+            peak = peaks[index]
+            state_tm1 = states_tm1[index]
+            output, state = cell.step(output_hm1, peak, state_tm1)
+            output_hm1 = output
+            states.extend(state)
+        return [output] + states
+
+    def __rnn(self, peaks, initial_states, indication=None):
+        """
+        wait to comp
+        """
+        if indication is None:
+            initial_output = K.zeros_like(initial_states[-1])
+            initial_output = T.unbroadcast(initial_output, 1)
+            if len(initial_states) > 0:
+                initial_states[0] = T.unbroadcast(initial_states[0], 1)
+            outputs, _ = theano.scan(self.__step,
+                                     sequences=[T.arange(self.max_time_steps)],
+                                     outputs_info=[initial_output] + initial_states,
+                                     non_sequences=peaks,
+                                     go_backwards=False)
+            ### WARNING !!! YOU CAN NOT PUT '[' and ']' around 'peaks' WHEN call THEANO.SCAN ###
+            # deal with Theano API inconsistency
+        else:
+            if len(initial_states) > 0:
+                initial_states[0] = T.unbroadcast(initial_states[0], 1)
+            outputs, _ = theano.scan(self.__step,
+                                     sequences=[T.arange(self.max_time_steps), indication],
+                                     outputs_info=[None] + initial_states,
+                                     non_sequences=peaks,
+                                     go_backwards=False)
+
+        if isinstance(outputs, list):
+            outputs = outputs[0]
+        outputs = T.squeeze(outputs)
+        axes = [1, 0] + list(range(2, outputs.ndim))
+        outputs = outputs.dimshuffle(axes)
+        return outputs
+
+    def build(self, input_shapes):
+        """
+        Assumption
+        """
+        self.__addCell(input_shapes)
+
+    @classmethod
+    def stack(cls, container, cell_obj):
+        """
+        Sequentially add a cell_obj to the container
+        """
+        raise NotImplementedError
+
+    def compute_output_shape(self, input_shapes):
+        """
+        wait to comp
+        """
+        if isinstance(input_shapes, list):
+            return (input_shapes[0][0], self.max_time_steps, self.output_dim)
+        else:
+            return (input_shapes[0], self.max_time_steps, self.output_dim)
+
+    def call(self, inputs):
+        """
+        wait to comp
+        """
         if self.cell_num == len(inputs):
             top_encoder_out, peaks = self.__preprocessInputs(inputs)
             initial_states = self.__getInitialStates(peaks)
@@ -408,7 +590,11 @@ class DecoderContainer(Layer):
         return outputs
 
     def get_config(self):
+        """
+        wait to comp
+        """
         config = {'max_time_steps': self.max_time_steps,
                   'decoder_config': self.decoder_config}
-        base_config = super(DecoderContainer, self).get_config()
+        base_config = super(AttentionDecoderContainer, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
+
