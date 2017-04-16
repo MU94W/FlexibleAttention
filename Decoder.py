d2ae5dc6ddf9bf599c63cb8b616b64e6f400653a
Mu Wang <nanqiao15@126.com>
2017-04-16 21:58:49 +0800

    use theano's identity

--- Decoder.py
+++ Decoder.py
@@ -300,8 +300,6 @@ class ResGRUCell(object):
             self.bias_r = None
             self.bias_h = None
         
-        self.res_kernel = K.ones((self.units,), name=self.name+'_res_kernel')
-
     #def step(self, inputs, states):
     def step(self, x, peak, state):
         h_tm1 = state # previous memory
@@ -319,7 +317,7 @@ class ResGRUCell(object):
         #hh = self.activation(x_h + K.dot(r * h_tm1, self.recurrent_kernel_h))
         hh = self.activation(x_h + r * (K.dot(h_tm1, self.recurrent_kernel_h) + K.dot(peak, self.peak_kernel_h)))
         h = z * h_tm1 + (1 - z) * hh
-        res_out = self.res_kernel * x + h
+        res_out = T.identity_like(x) + h
         return res_out, [res_out]
     
     def getInitialState(self, peak):
